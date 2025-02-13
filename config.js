4f94f428e3ca65792873728fc29082348ae0287c
novaroxas <jeremy.jimmerson@live.com>
2025-02-13 11:32:22 -1000

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you love me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No? Thats crazy. Let me go find a ROPE.",                                               // Text for "No" button
-            secretAnswer: "I'll kill myself right now. ❤️"           // Secret hover message
+            secretAnswer: "SUPER TURBO GIGA YES. ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
