cf8fc6f82d1c0382e88d3c43b9be57a3d43c795a
Nihar Deshpande <nihar.deshpande1810@gmail.com>
2026-02-01 16:32:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/djedtmipm/video/upload/v1769943558/Yun_Hi_Tanu_Weds_Manu_320_Kbps_g0wirk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
