a500e866107ce004c45cb4a31fec516bda521540
ANUBHAV NAYAK <anubhavnayakrocky05@gmail.com>
2026-02-01 15:31:48 +0530

    Update secret answer and love meter messages

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: " nonono I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Sola re ete bhala paisu mate ?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "besi bhal paibar katha chadiiii uda hela takkk! 🚀💝",              // Shows when they go past 1000%
+        normal: "pls moree fuck  🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Happppyyyyyyyy Valentines Day BAbeeeeeeeeeeeeeeeeeeeeee chadddiiii phatigaalllllaaa udaaa bhi heigalllalallaal balalalllallalallallallalallal",
+        message: "Happppyyyyyyyy Valentines Day BAbeeeeeeeeeeeeeeeeeeeeee       chadddiiii phatigaalllllaaa udaaa bhi heigalllalallaal balalalllallalallallallalallal",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
