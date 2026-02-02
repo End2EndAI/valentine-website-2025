ed5aece2542fe1973e0c331c1ff5863735a321f7
galaaarshi <133569634+galaaarshi@users.noreply.github.com>
2026-02-02 09:51:36 -0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['💖', '💗']  // Heart emojis
+        hearts: ['💖', '💗'],  // Heart emojis
         bears: ['🦔', '🐧']                       // Cute bear emojis
     },
 
