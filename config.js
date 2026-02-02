bda40a8a6ce4e55cc289085e5f308d733d6990c6
adamloro3com-gif <adamloro3.com@gmail.com>
2026-02-01 19:00:55 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/divnp6lqj/video/upload/v1769989642/Mac_Miller_-_Congratulations_feat._Bilal_No_Intro_jaxmzx.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.4                           // Volume level (0.0 to 1.0)
