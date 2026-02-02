692cbbee9fa7cc5382b89b425c7cfd992f6a1920
adamloro3com-gif <adamloro3.com@gmail.com>
2026-02-01 18:59:34 -0800

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
         },
         second: {
             text: "How much do you love me? 👉👈🥺",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
+            startText: "This muchieee!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get a big warm hug and a huge kiss!",
         emojis: "💖💝💋❤️💕"  // These will bounce around
     },
 
