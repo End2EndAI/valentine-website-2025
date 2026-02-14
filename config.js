b23a63d5933db8fa4f0ad83c6d35add4ae100489
TALENT-EDIT <bookmichaeldare@gmail.com>
2026-02-14 09:27:33 +0100

    Update config.js1

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "HELEN-MY SOFT",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! By clicking yes you agree to: 🎉💝💖💝💓",
+        message: "A mini gift, A pool date, A movie date, Unlimited forehead kisses",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
